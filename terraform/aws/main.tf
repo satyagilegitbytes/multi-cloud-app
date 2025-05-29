3e35966f516a45b7dcefb8db0b2caffb92d1ac52
satyagilegitbytes <sarthaktyagi31@gmail.com>
2025-05-29 08:41:13 +0530

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,13 +1,9 @@
 provider "aws" {
+  region = "us-east-1"
   access_key = "test"
   secret_key = "test"
-  region     = "us-east-1"
 
   endpoints {
     s3 = "http://localhost:4566"
   }
-}
-
-resource "aws_s3_bucket" "local_bucket" {
-  bucket = "demo-local-bucket"
 }
