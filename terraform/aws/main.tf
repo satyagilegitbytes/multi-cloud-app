431b633dbe7bc191332ed0f1bd3c87fcefd47c97
satyagilegitbytes <sarthaktyagi31@gmail.com>
2025-05-30 08:54:39 +0530

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -4,23 +4,14 @@ provider "aws" {
   secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
-  s3_force_path_style         = true
 
   endpoints {
     s3  = "http://localhost:4566"
     sts = "http://localhost:4566"
   }
-}
 
-resource "random_id" "bucket_suffix" {
-  byte_length = 4
-}
-
-resource "aws_s3_bucket" "sarthak_bucket" {
-  bucket = "sarthak-${random_id.bucket_suffix.hex}"
-}
-
-resource "aws_s3_bucket_acl" "sarthak_bucket_acl" {
-  bucket = aws_s3_bucket.sarthak_bucket.id
-  acl    = "private"
+  # Use a nested config block for SDK options
+  config {
+    s3_force_path_style = true
+  }
 }
