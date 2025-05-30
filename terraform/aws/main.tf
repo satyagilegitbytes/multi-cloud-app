1dd9762b773d2fa5efc6bdd4363d8f547328add7
satyagilegitbytes <sarthaktyagi31@gmail.com>
2025-05-30 08:53:19 +0530

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,10 +1,14 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "test"
-  secret_key = "test"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
 
   endpoints {
-    s3 = "http://localhost:4566"
+    s3  = "http://localhost:4566"
+    sts = "http://localhost:4566"
   }
 }
 
@@ -14,5 +18,9 @@ resource "random_id" "bucket_suffix" {
 
 resource "aws_s3_bucket" "sarthak_bucket" {
   bucket = "sarthak-${random_id.bucket_suffix.hex}"
+}
+
+resource "aws_s3_bucket_acl" "sarthak_bucket_acl" {
+  bucket = aws_s3_bucket.sarthak_bucket.id
   acl    = "private"
 }
